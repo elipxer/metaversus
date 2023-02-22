6e3dae1971bc681fdb0562b0d8022a1626353351
@elipe <eliassalesdeoliveira@gmail.com>
2023-02-22 01:19:19 -0400

    hero up

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -12,7 +12,23 @@ const Hero = () => (
       initial="hidden"
       whileInView="show"
       viewport={{ once: false, amount: 0.25 }}
-    />
+      className={`${styles.innerWidth} mx-auto flex-col`}
+    >
+      <div className="flex justify-center items-center flex-col relative z-10">
+        <motion.h1 variants={textVariant(1.1)}
+          className="{styles.heroHeading}"
+        >
+          Metaverse
+        </motion.h1>
+        <motion.div
+          variants={textVariant(1.2)}
+          className="flex flex-row justify-center items-center"
+        >
+          <h1 className={styles.heroHeading}>Ma</h1>
+          <div className={styles.heroDText}></div>
+        </motion.div>
+      </div>
+    </motion.div>
   </section>
 );
 
