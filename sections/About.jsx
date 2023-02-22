e6061a5926e48e3cbb2bc9173e8353efeb8d6830
@elipe <eliassalesdeoliveira@gmail.com>
2023-02-22 01:46:47 -0400

    aboutup3

--- sections/About.jsx
+++ sections/About.jsx
@@ -8,7 +8,13 @@ import { fadeIn, staggerContainer } from '../utils/motion';
 
 const About = () => (
   <section className={`${styles.paddings} relative z-10`}>
-    <div />
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{}}
+    />
     About section
   </section>
 );
